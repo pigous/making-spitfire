795acba42f7ce6e1366a240b939bbee23d3b97d4
Márk András BALOG <markandras.balog@nng.com>
2018-03-16 21:27:24 +0000

    routes added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get("/", function(req, res){
     res.render("landing");
 });
 
+router.get("/contact", function(req, res){
+    res.render("contact/contact");
+});
+
+router.get("/discover", function(req, res){
+    res.render("discover/discover");
+});
+
+router.get("/inform", function(req, res){
+    res.render("inform/inform");
+});
+
+router.get("/project", function(req, res){
+    res.render("project/project");
+});
+
+router.get("/story", function(req, res){
+    res.render("story/story");
+});
+
+router.get("/team", function(req, res){
+    res.render("team/team");
+});
+
 module.exports = router;
